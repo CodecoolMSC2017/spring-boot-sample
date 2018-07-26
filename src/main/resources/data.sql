8ec7c817ada9c5a78a06bc6cbdfb04e286b9cc49
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-07-26 08:22:43 +0200

    Add user with password user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,7 @@
-insert into users (username, password, enabled) values ('admin', '$2a$10$2Gi3G9XaKalERoIa74OYruEHZyqSUqn10uSiOzk4PvOgL49vejna.', true);
-insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
+insert into users (username, password, enabled) values
+    ('admin', '$2a$10$2Gi3G9XaKalERoIa74OYruEHZyqSUqn10uSiOzk4PvOgL49vejna.', true),
+    ('user', '$2a$10$FKtRACKTCAsvoiV6XhCUtuff31QpzfdzSjM1JgyXWRAy9APgTW5la', true);
+
+insert into authorities (username, authority) values
+    ('admin', 'ROLE_ADMIN'),
+    ('user', 'ROLE_USER');
